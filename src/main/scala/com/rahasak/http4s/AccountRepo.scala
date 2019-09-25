bd2ee88a1c78680307b4aa2a1800fabbc7731a3c
eranga bandara <eranga.herath@pagero.com>
2019-09-24 20:09:09 -0400

    account repo

---
+++ src/main/scala/com/rahasak/http4s/AccountRepo.scala
@@ -0,0 +1,16 @@
+package com.rahasak.http4s
+
+import cats.effect.IO
+
+
+trait AccountRepo {
+  def createAccount(account: Account): IO[Int]
+
+  def updateAccount(id: String, account: Account): IO[Int]
+
+  def getAccount(id: String): IO[Option[Account]]
+
+  def getAccounts(): IO[List[Account]]
+}
+
+
