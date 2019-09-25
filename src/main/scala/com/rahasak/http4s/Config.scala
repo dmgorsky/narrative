5c5fbfdbf5fe79649557c586cec105c4507ba9b9
eranga bandara <eranga.herath@pagero.com>
2019-09-24 22:16:53 -0400

    done with config

--- src/main/scala/com/rahasak/http4s/Config.scala
+++ src/main/scala/com/rahasak/http4s/Config.scala
@@ -1,18 +1,20 @@
 package com.rahasak.http4s
 
 import cats.effect.IO
-import com.zaxxer.hikari.HikariConfig
+import io.circe.config.parser
+import io.circe.generic.auto._
+
+case class ServerConfig(port: Int, host: String)
+
+case class DbConfig(url: String, username: String, password: String, poolSize: Int)
+
+case class Config(serverConfig: ServerConfig, dbConfig: DbConfig)
 
 object Config {
-  def load(conf: String = "application.conf") = {
-    IO {
-      val config = new HikariConfig()
-      config.setJdbcUrl("jdbc:mysql://localhost:3306/mystiko")
-      config.setUsername("root")
-      config.setPassword("root")
-      config.setMaximumPoolSize(5)
-
-      config
-    }
+  def load() = {
+    for {
+      dbConf <- parser.decodePathF[IO, DbConfig]("db")
+      serverConf <- parser.decodePathF[IO, ServerConfig]("server")
+    } yield Config(serverConf, dbConf)
   }
 }
