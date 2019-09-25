2d02a00772b7f3768f32168b19c3ab1551b177b2
eranga bandara <eranga.herath@pagero.com>
2019-09-24 21:34:20 -0400

    post, put handle with error

--- src/main/scala/com/rahasak/http4s/DocumentRoutes.scala
+++ src/main/scala/com/rahasak/http4s/DocumentRoutes.scala
@@ -15,11 +15,11 @@ object DocumentRoutes {
       case req@POST -> Root / "accounts" =>
         req.decode[Account] { acc =>
           documentRepo.createAccount(acc).flatMap(id => Created(id))
-        }
+        }.handleErrorWith(e => BadRequest(e.getMessage))
       case req@PUT -> Root / "accounts" / id =>
         req.decode[Account] { acc =>
           documentRepo.updateAccount(id, acc).flatMap(_ => Accepted())
-        }
+        }.handleErrorWith(e => BadRequest(e.getMessage))
       case _@GET -> Root / "accounts" =>
         documentRepo.getAccounts().flatMap(accs => Ok(accs))
       case _@GET -> Root / "accounts" / id =>
